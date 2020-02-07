1c5e5459e0c09858d551513cf576e40b87d01336
User Name <artur.it2914@gmail.com>
2020-02-07 18:42:00 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', function(req, res, next){
+    req.count = 0;
+	next();
+},
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	next();
+}
+,
+    function(req, res, next){
+        req.count++;
+	res.send(`${req.count}`);
+}
+);
+
+app.listen(3000);
